6f631c73f977e35292c42a827e3d0b0d6a123ce9
LonelyWhistle <smartechsrls2020@gmail.com>
2025-11-19 12:09:06 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,30 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    // Risolve l'errore "import.meta" impostando un target di compilazione moderno.
-    target: 'esnext' 
-  }
+    // Aumentiamo il limite dell'avviso a 3MB (3000kb) per non vedere l'avviso
+    chunkSizeWarningLimit: 3000, 
+    rollupOptions: {
+      output: {
+        // Questa funzione spezza il codice in file più piccoli per ottimizzare il caricamento
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // Mette la libreria XLSX in un file separato
+            if (id.includes('xlsx')) {
+              return 'xlsx';
+            }
+            // Mette la libreria PDF in un file separato
+            if (id.includes('pdfjs-dist')) {
+              return 'pdfjs';
+            }
+            // Mette Firebase in un file separato
+            if (id.includes('firebase')) {
+              return 'firebase';
+            }
+            // Tutto il resto va in 'vendor'
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
 })
-
